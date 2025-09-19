88619ef707fe6701f320d9f280ab120652bd7fda
Jiarui1111 <xiangjiarui1111@gmail.com>
2025-09-19 16:14:42 +0800

    file creation

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URLS = {
+    piperx: "https://api.goldsky.com/api/public/project_clzxbl27v2ce101zr2s7sfo05/subgraphs/story-dex-swaps-mainnet/prod/gn",
+    storyhunt: "https://api.goldsky.com/api/public/project_clzxbl27v2ce101zr2s7sfo05/subgraphs/story-dex-swaps-mainnet/1.0.23/gn"
+  };
