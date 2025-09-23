7431320d1372e4e4352b95f398a36681f6635e5e
wanyi <xiangjiarui1111@gmail.com>
2025-09-23 18:11:18 +0800

    update txcount

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import tradesRouter from './routes/trades';
 import userStatsRouter from './routes/users';
 import whaleRouter from './routes/whales';
 import hfRouter from './routes/highfrequencys';
+import txRouter from './routes/txcount';
 const app = new Hono();
 
 app.route('/api/trades', tradesRouter);
 app.route('/api/users', userStatsRouter);
 app.route('/api/whales', whaleRouter);
 app.route('/api/highfrequencys', hfRouter);
+app.route('/api/txcount', txRouter);
 
 app.get('/', (c) => c.text('PiperX API is running 🚀'));
 
